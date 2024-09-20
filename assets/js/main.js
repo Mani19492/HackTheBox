41f528adf273fe1379e4cadea87c5643f29ee9b1
Mani19492 <23eg509a04@anurag.edu.in>
2024-09-20 09:06:18 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,38 +121,34 @@
     /* ========  themeSwitcher start ========= */
 
   // themeSwitcher
-  const themeSwitcher = document.getElementById('themeSwitcher');
+const themeSwitcher = document.getElementById('themeSwitcher');
 
-  // Theme Vars
-  const userTheme = localStorage.getItem('theme');
-  const systemTheme = window.matchMedia('(prefers-color0scheme: dark)').matches;
+// Theme Vars
+const userTheme = localStorage.getItem('theme');
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-  // Initial Theme Check
-  const themeCheck = () => {
-    if (userTheme === 'dark' || (!userTheme && systemTheme)) {
-      document.documentElement.classList.add('dark');
-      return;
-    }
-  };
+// Initial Theme Check
+const themeCheck = () => {
+  if (userTheme === 'dark' || (!userTheme && systemTheme)) {
+    document.documentElement.classList.add('dark');
+  }
+};
 
-  // Manual Theme Switch
-  const themeSwitch = () => {
-    if (document.documentElement.classList.contains('dark')) {
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
-      return;
-    }
-
+// Manual Theme Switch
+const themeSwitch = () => {
+  // Only switch to dark mode if it's not already set
+  if (!document.documentElement.classList.contains('dark')) {
     document.documentElement.classList.add('dark');
     localStorage.setItem('theme', 'dark');
-  };
+  }
+};
 
-  // call theme switch on clicking buttons
-  themeSwitcher.addEventListener('click', () => {
-    themeSwitch();
-  });
+// Call theme switch on clicking button
+themeSwitcher.addEventListener('click', () => {
+  themeSwitch();
+});
 
-  // invoke theme check on initial load
-  themeCheck();
-  /* ========  themeSwitcher End ========= */
+// Invoke theme check on initial load
+themeCheck();
+/* ========  themeSwitcher End ========= */
 })();
